29955b7b673b110d5a04c01e7d981f4ade6b2437
JunHyuk <ericyoo0107@gmail.com>
2023-05-11 21:32:51 +0900

    initial commit

---
+++ src/main/java/com/example/junhyuk_board/JunhyukBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.junhyuk_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunhyukBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunhyukBoardApplication.class, args);
+    }
+
+}
